00d594f20819eec75f2f6dc9afa06a52db8497b5
Pedro Gomez <makhnnar@gmail.com>
2021-03-19 15:59:17 -0400

    Update QuestionDetailsFragment.java
    :x: deleted unnecesary login on detail question fragment

--- tutorial_android_application/src/main/java/com/techyourchance/unittesting/screens/questiondetails/QuestionDetailsFragment.java
+++ tutorial_android_application/src/main/java/com/techyourchance/unittesting/screens/questiondetails/QuestionDetailsFragment.java
@@ -7,15 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.techyourchance.unittesting.questions.FetchQuestionDetailsUseCase;
-import com.techyourchance.unittesting.questions.QuestionDetails;
 import com.techyourchance.unittesting.screens.common.controllers.BaseFragment;
-import com.techyourchance.unittesting.screens.common.screensnavigator.ScreensNavigator;
-import com.techyourchance.unittesting.screens.common.toastshelper.ToastsHelper;
 
-public class QuestionDetailsFragment extends BaseFragment implements
-        FetchQuestionDetailsUseCase.Listener,
-        QuestionDetailsViewMvc.Listener {
+public class QuestionDetailsFragment extends BaseFragment {
 
     private static final String ARG_QUESTION_ID = "ARG_QUESTION_ID";
 
@@ -26,10 +20,6 @@ public class QuestionDetailsFragment extends BaseFragment implements
         fragment.setArguments(args);
         return fragment;
     }
-
-    private FetchQuestionDetailsUseCase mFetchQuestionDetailsUseCase;
-    private ToastsHelper mToastsHelper;
-    private ScreensNavigator mScreensNavigator;
 
     private QuestionDetailsViewMvc mViewMvc;
 
@@ -44,53 +34,24 @@ public class QuestionDetailsFragment extends BaseFragment implements
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        mFetchQuestionDetailsUseCase = getCompositionRoot().getFetchQuestionDetailsUseCase();
-        mToastsHelper = getCompositionRoot().getToastsHelper();
-        mScreensNavigator = getCompositionRoot().getScreensNavigator();
         mViewMvc = getCompositionRoot().getViewMvcFactory().getQuestionDetailsViewMvc(container);
-
         mQuestionDetailsController.bindView(mViewMvc);
-        mQuestionDetailsController.bindQuestionId(getArguments().getString(ARG_QUESTION_ID));
-
+        mQuestionDetailsController.bindQuestionId(
+                getArguments().getString(ARG_QUESTION_ID)
+        );
         return mViewMvc.getRootView();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mFetchQuestionDetailsUseCase.registerListener(this);
-        mViewMvc.registerListener(this);
-
-        mViewMvc.showProgressIndication();
-        mFetchQuestionDetailsUseCase.fetchQuestionDetailsAndNotify(getQuestionId());
+        mQuestionDetailsController.onStart();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mFetchQuestionDetailsUseCase.unregisterListener(this);
-        mViewMvc.unregisterListener(this);
+        mQuestionDetailsController.onStop();
     }
 
-    private String getQuestionId() {
-        return getArguments().getString(ARG_QUESTION_ID);
-    }
-
-    @Override
-    public void onQuestionDetailsFetched(QuestionDetails questionDetails) {
-        mViewMvc.hideProgressIndication();
-        mViewMvc.bindQuestion(questionDetails);
-    }
-
-    @Override
-    public void onQuestionDetailsFetchFailed() {
-        mViewMvc.hideProgressIndication();
-        mToastsHelper.showUseCaseError();
-    }
-
-    @Override
-    public void onNavigateUpClicked() {
-        mScreensNavigator.navigateUp();
-    }
 }
